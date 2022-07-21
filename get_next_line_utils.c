b35758520f6ee86546d6303d2dfd3e04a032ad56
timothee <94700601+t-mercier@users.noreply.github.com>
2022-07-21 11:21:04 +0200

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -15,24 +15,20 @@
 size_t	ft_strlen(char *s)
 {
 	size_t	i;
-
-	i = 0;
-	while (s && s[i])
-		i++;
+	
+	i = -1;
+	while(s[++i]);
 	return (i);
 }
 
 char	*ft_strcpy(char *dst, char *src)
 {
-	unsigned int	len;
+	size_t	i;
 
-	len = ft_strlen(src);
-	dst[len] = '\0';
-	while ((len) > 0)
-	{
-		len--;
-		dst[len] = src[len];
-	}
+	i = -1;
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = '\0';
 	return (dst);
 }
 
